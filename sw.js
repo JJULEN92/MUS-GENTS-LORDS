44a8abfa729099eee769f188ca930e313fc70b58
JJULEN92 <32551752+JJULEN92@users.noreply.github.com>
2026-05-26 12:47:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "mus-gents-lords-v2";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+const CACHE_NAME = "mus-gents-lords-v081";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./tournament-icon.png", "./hero-bg.png" ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
